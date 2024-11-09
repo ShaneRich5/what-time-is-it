c80c7ad695dc36253436b9c48db3929810250604
Shane Richards <shane.richards121@gmail.com>
2024-11-08 20:52:58 -0500

    feat: adding a count down

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,44 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-const TIMER_INTERVAL = 1000
+const TIMER_INTERVAL = 1000; // every second
 
 const App = () => {
-  const [date, setDate] = useState(new Date())
+  const [date, setDate] = useState(new Date());
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setDate(new Date())
-    }, TIMER_INTERVAL)
+      setDate(new Date());
+    }, TIMER_INTERVAL);
 
     return () => {
-      clearInterval(timer)
-    }
-  })
+      clearInterval(timer);
+    };
+  });
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        {date.toLocaleTimeString()}
-      </h1>
+      <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+          <span className="countdown font-mono text-5xl">
+            <span style={{ "--value": date.getHours() }}></span>
+          </span>
+          hours
+        </div>
+        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+          <span className="countdown font-mono text-5xl">
+            <span style={{ "--value": date.getMinutes() }}></span>
+          </span>
+          min
+        </div>
+        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+          <span className="countdown font-mono text-5xl">
+            <span style={{ "--value": date.getSeconds() }}></span>
+          </span>
+          sec
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
