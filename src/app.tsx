16dbf3e98a0c82309c7c396bcdc8a0a47b6f9035
Shane Richards <shane.richards121@gmail.com>
2024-11-08 20:45:30 -0500

    feat: adding a live timer

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react"
+
+const TIMER_INTERVAL = 1000
+
 const App = () => {
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDate(new Date())
+    }, TIMER_INTERVAL)
+
+    return () => {
+      clearInterval(timer)
+    }
+  })
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        {date.toLocaleTimeString()}
+      </h1>
+    </div>
   )
 }
 
