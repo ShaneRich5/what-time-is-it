c034ecd1a866340cd69facbb51e0e91c512361e6
Shane Richards <shane.richards121@gmail.com>
2024-11-08 22:26:26 -0500

    feat: changing the styling on the timer

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { CSSProperties, useEffect, useState } from "react";
 
 export interface CSSPropertiesWithVars extends CSSProperties {
-  '--value': number;
+  "--value": number;
   // any other vars you may use
 }
 
@@ -23,21 +23,27 @@ const App = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
-        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <div className="flex flex-col">
           <span className="countdown font-mono text-5xl">
-            <span style={{ "--value": date.getHours() } as CSSPropertiesWithVars}></span>
+            <span
+              style={{ "--value": date.getHours() } as CSSPropertiesWithVars}
+            ></span>
           </span>
           hours
         </div>
-        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <div className="flex flex-col">
           <span className="countdown font-mono text-5xl">
-            <span style={{ "--value": date.getMinutes() } as CSSPropertiesWithVars}></span>
+            <span
+              style={{ "--value": date.getMinutes() } as CSSPropertiesWithVars}
+            ></span>
           </span>
           min
         </div>
-        <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+        <div className="flex flex-col">
           <span className="countdown font-mono text-5xl">
-            <span style={{ "--value": date.getSeconds() } as CSSPropertiesWithVars}></span>
+            <span
+              style={{ "--value": date.getSeconds() } as CSSPropertiesWithVars}
+            ></span>
           </span>
           sec
         </div>
